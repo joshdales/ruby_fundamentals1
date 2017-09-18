6b70bab8400dfb6c970e4bc2c94aa00b04e95801
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-18 14:26:05 -0400

    Ask for age

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
 puts "What is your name?"
 user_name = gets
 puts "Hello, #{user_name}"
+
+puts "What is your age?"
+user_age = gets
+puts "Well #{user_name} I think that #{user_age} is a nice age."
