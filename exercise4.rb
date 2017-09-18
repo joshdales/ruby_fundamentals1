1980d289bd7064cc4f1b3f14d51e74c9f2869a57
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-18 15:19:49 -0400

    Ask for name and respond based on length

--- exercise4.rb
+++ exercise4.rb
@@ -18,3 +18,15 @@ elsif user_age > my_age && user_age < 105
 else
   puts "I'm not sure I believe you."
 end
+
+
+puts "What is your name?"
+user_name = gets.chop
+
+if user_name.length > 10
+  puts "hi #{user_name}"
+elsif user_name.length < 10
+  puts "hello #{user_name}"
+else
+  puts "hey #{user_name}"
+end
