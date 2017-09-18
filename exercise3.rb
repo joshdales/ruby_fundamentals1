5e21f2a7c4073a8ad068071d6464972467357608
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-18 14:26:47 -0400

    change gets to gets.chomp

--- exercise3.rb
+++ exercise3.rb
@@ -1,7 +1,7 @@
 puts "What is your name?"
-user_name = gets
+user_name = gets.chomp
 puts "Hello, #{user_name}"
 
 puts "What is your age?"
-user_age = gets
+user_age = gets.chomp
 puts "Well #{user_name} I think that #{user_age} is a nice age."
