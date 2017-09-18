0f2e2c05e96c23c3dac7c2b272fc6a3dbe6b156c
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-18 16:46:46 -0400

    fix energy system

--- exercise5.rb
+++ exercise5.rb
@@ -1,26 +1,40 @@
 distance = 0
-energy = 50
+energy = 20
 
 
-puts "you are at home, about to go on a 100km run"
+puts "you are at home, about to go on a 50km run"
 puts "if you want to go home just say 'go home'"
 
-while distance < 100
+while distance < 50
   puts "Would you like to walk or run?"
   choice = gets.chomp
+
+if energy > 0
   if choice == "run"
     distance += 5
-    energy -= 10
-    puts "Distance from home is #{distance}"
+    energy -= 5
+    puts "Distance from home is #{distance}km."
   elsif choice == "walk"
     distance += 1
-    energy += 3
-    puts "Distance from home is #{distance}"
-  elsif energy = 0
-    puts "you ran out of energy, you should walk or go home"
+    energy += 5
+    puts "Distance from home is #{distance}km."
   end
+end
+
+  if energy <= 0
+    puts "you ran out of energy, you should walk or go home."
+    if choice == "run"
+      puts "you don't have the energy to run."
+    elsif choice == "walk"
+      distance += 1
+      energy += 5
+      puts "Distance from home is #{distance}km."
+    end
+  end
+
   if choice == "go home"
-    puts "you went the #{distance}km home"
+    puts "you went the #{distance}km home."
     break
   end
+
 end
