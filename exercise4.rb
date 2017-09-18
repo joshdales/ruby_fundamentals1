49a4f2f72a70c89415accd04ea3999face943e01
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-18 14:57:28 -0400

    Ask for age and give facts comparing ages

--- exercise4.rb
+++ exercise4.rb
@@ -6,3 +6,15 @@ if user_number > 100
 else
   puts "why not dream a little bigger?"
 end
+
+puts "What is your age"
+user_age = gets.chomp.to_i
+my_age = 27
+
+if user_age < my_age
+  puts "Did you know that you are #{my_age - user_age} years younger than me"
+elsif user_age > my_age && user_age < 105
+  puts "Did you know that you are #{user_age - my_age} years older than me?"
+else
+  puts "I'm not sure I believe you."
+end
