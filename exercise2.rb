8ab519c8fe2828b21cba0a0298a17f20f63a0de3
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-18 13:57:23 -0400

    add integer to string

--- exercise2.rb
+++ exercise2.rb
@@ -2,4 +2,6 @@ meal = 55.0
 tip = (meal / 100) * 20
 puts tip #Calculating a good tip
 
-puts "meal = $55" + tip #Adding + operator (sting and int)- cost of meal with tip
+tip.to_s #Convert integer to string
+
+puts "meal = $55 + " + tip.to_s #Adding + operator (string and integer)- cost of meal with tip
