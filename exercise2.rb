08cb47dd10065cee64358de70a80f3f8675d48f7
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-18 14:00:22 -0400

    String interpolation

--- exercise2.rb
+++ exercise2.rb
@@ -5,3 +5,5 @@ puts tip #Calculating a good tip
 tip.to_s #Convert integer to string
 
 puts "meal = $55 + " + tip.to_s #Adding + operator (string and integer)- cost of meal with tip
+
+puts "What is the total of 45628 multiplied by 7839? Why it's #{45628*7839} of course!" #String interpolation
