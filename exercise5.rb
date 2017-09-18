0a2c80f9adf8fbfaa88b6faed8f047508988ed64
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-18 17:03:33 -0400

    Add ability to eat, and a goal of 50km

--- exercise5.rb
+++ exercise5.rb
@@ -2,34 +2,43 @@ distance = 0
 energy = 20
 
 
-puts "you are at home, about to go on a 50km run"
-puts "if you want to go home just say 'go home'"
+puts "You are at home, about to try and run 50km\nIf you want to go home just say 'go home'\n"
 
 while distance < 50
   puts "Would you like to walk or run?"
   choice = gets.chomp
 
-if energy > 0
-  if choice == "run"
-    distance += 5
-    energy -= 5
-    puts "Distance from home is #{distance}km."
-  elsif choice == "walk"
-    distance += 1
-    energy += 5
-    puts "Distance from home is #{distance}km."
+  if energy > 0
+    if choice == "run"
+      distance += 5
+      energy -= 5
+      puts "Distance travelled is #{distance}km."
+    elsif choice == "walk"
+      distance += 1
+      energy += 5
+      puts "Distance travelled is #{distance}km."
+    end
   end
-end
 
   if energy <= 0
-    puts "you ran out of energy, you should walk or go home."
+    puts "you ran out of energy, you should rest or go home."
     if choice == "run"
       puts "you don't have the energy to run."
     elsif choice == "walk"
       distance += 1
       energy += 5
-      puts "Distance from home is #{distance}km."
+      puts "Distance travelled is #{distance}km."
     end
+  end
+
+  if choice == "eat"
+    energy += 10
+    puts "Yum!"
+  end
+
+  if distance >= 50
+    puts "What a run! Good work."
+    break
   end
 
   if choice == "go home"
